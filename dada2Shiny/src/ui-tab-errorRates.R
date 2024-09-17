8c0c12f723583cdb8541a56bbb4ba71b04a35427
Nabil Rahiman <nabil.rahiman@gmail.com>
2024-09-17 14:33:31 +0400

    Update ui-tab-errorRates.R

--- dada2Shiny/src/ui-tab-errorRates.R
+++ dada2Shiny/src/ui-tab-errorRates.R
@@ -10,7 +10,8 @@ tabItem(
             column(
                 12,
                 p('The plots display the results of the DADA2 error model, which compares the observed error frequencies (gray points) for specific nucleotide substitutions (e.g., A to C, G to T) against the predicted error rates (red line) based on the quality scores of the reads.'),
-                p('By learning the error patterns, DADA2 can more accurately distinguish between true biological sequences and sequencing errors, improving the overall accuracy of sequence analysis. This model plays a key role in denoising the data for downstream analysis.')
+                p('By learning the error patterns, DADA2 can more accurately distinguish between true biological sequences and sequencing errors, improving the overall accuracy of sequence analysis. This model plays a key role in denoising the data for downstream analysis.'),
+                p('For a more in depth explanation on how to interpret these plots, please refer to the DADA2 tutorial here [https://benjjneb.github.io/dada2/index.html]')
             ),
             column(
                 6,
